9c7aa909792f27919aa50fe86ffdf936aaac1ce2
Llnavarreteg23 <lnavarretegarces@gmail.com>
2025-03-27 17:32:11 -0500

    Laboratorio de objetos 1

--- 4-termo.js
+++ 4-termo.js
@@ -1,7 +1,18 @@
+const prompt = require("prompt-sync")();
 function Termometro() {
+
+    this.actualizar = function actualizar(grados) {
+        if (grados > 30) {
+            console.log(`Hace calor!`);
+        } else if (grados < 10) {
+        console.log(`Hace frio!`);
+        } else {
+        console.log(`Temperatura agradable!`);
+        }
+    }
 }
 
-Termometro.prototype.actualizar = function actualizar(grados) {
+/*Termometro.prototype.actualizar = function actualizar(grados) {
     if (grados > 30) {
         console.log(`Hace calor!`);
     } else if (grados < 10) {
@@ -10,5 +21,7 @@ Termometro.prototype.actualizar = function actualizar(grados) {
     console.log(`Temperatura agradable!`);
     }
 }
+*/
 let termometro1 = new Termometro();
-termometro1.actualizar(25)
+grados = Number(prompt("Digite la temperatura: "))
+termometro1.actualizar(grados)
